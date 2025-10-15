9640e91a8e685e935dff0aeb2ef590e07681702b
Amanda <soganileamanda@gmail.com>
2025-10-15 19:16:16 +0200

    Created welcome page content with image of logo and tagline and created a button leading to the home screen

--- FarmCare/WelcomePage.swift
+++ FarmCare/WelcomePage.swift
@@ -8,8 +8,48 @@
 import SwiftUI
 
 struct WelcomePage: View {
+    @State private var ShowingText = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            VStack(spacing: 60) {
+                Spacer()
+                
+                Image("FarmCare Transparent")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                
+                
+                
+                Text("Modern Tools For Traditional Farmers")
+                    .font(.title3)
+                    .fontWeight(.heavy)
+                    .padding()
+                
+                Spacer()
+                
+                NavigationLink { ContentView()
+                    
+                } label: {
+                    
+                    ZStack {
+                        
+                        RoundedRectangle(cornerRadius: 40)
+                            .frame(width: 360, height: 85)
+                            .foregroundStyle(.black.gradient)
+                        
+                        Text("Get Started")
+                            .font(.title2)
+                            .fontDesign(.default)
+                            .fontWeight(.bold)
+                            .foregroundStyle(Color.white)
+                           
+                    }
+                }
+                .buttonStyle(.plain)
+                
+            }
+        }
     }
 }
 
