b01cb2a581e1efdd6f05f471b44835d4fbeaef5c
Amanda <soganileamanda@gmail.com>
2025-10-15 12:10:28 +0200

    Initial Commit

---
+++ FarmCare/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FarmCare
+//
+//  Created by Amanda Soganile on 15/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FarmCare/FarmCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  FarmCareApp.swift
+//  FarmCare
+//
+//  Created by Amanda Soganile on 15/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct FarmCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
