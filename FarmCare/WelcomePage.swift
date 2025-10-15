20c87e5bfd2b60a398a4cde966f10acb46c367c0
Amanda <soganileamanda@gmail.com>
2025-10-15 16:55:56 +0200

    Added a welcome page

---
+++ FarmCare/WelcomePage.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomePage.swift
+//  FarmCare
+//
+//  Created by Amanda Soganile on 15/10/2025.
+//
+
+import SwiftUI
+
+struct WelcomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomePage()
+}
