836039bff74439e69196de9d4e1fe5468382737c
Amanda <soganileamanda@gmail.com>
2025-10-16 11:58:19 +0200

    Created a tab view with three tabs, one for home, one for adding a new animal and one for viewing the animals

---
+++ FarmCare/tabview.swift
@@ -0,0 +1,33 @@
+//
+//  tabview.swift
+//  FarmCare
+//
+//  Created by Amanda Soganile on 16/10/2025.
+//
+
+import SwiftUI
+
+struct tabview: View {
+    var body: some View {
+        TabView {
+            ContentView()
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            
+            add_New_Animal()
+                .tabItem {
+                    Label("Add", systemImage: "plus.app.fill")
+                }
+            
+            animal_species_tab()
+                .tabItem {
+                    Label("Animal", systemImage: "pawprint.fill")
+                }
+        }
+    }
+}
+
+#Preview {
+    tabview()
+}
