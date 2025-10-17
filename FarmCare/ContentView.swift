88ad233640a046bd2bf39a34ec7e51b4cecc6b2e
Amanda <soganileamanda@gmail.com>
2025-10-17 10:00:30 +0200

    new adjustments to add animal page, save button added

--- FarmCare/ContentView.swift
+++ FarmCare/ContentView.swift
@@ -9,16 +9,65 @@ import SwiftUI
 
 
 struct ContentView: View {
+    @State private var ShowingText = false
+    @State private var farmLogo: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            VStack(spacing: 60) {
+                Spacer()
+                
+                Image("FarmCare Transparent")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                    .scaleEffect(farmLogo ? 1.0 : 0.8)
+                    .opacity(farmLogo ? 1.0 : 0.0)
+                    .animation(.easeInOut(duration: 1.5), value: farmLogo)
+                    
+                
+                
+                
+                Text("Modern Tools For Traditional Farmers")
+                    .font(.title3)
+                    .fontWeight(.heavy)
+                    .foregroundStyle(Color(red: 7/255, green: 28/255, blue: 24/255))
+//                    .fontDesign(.rounded)
+                    .padding()
+                
+                
+                Spacer()
+                
+                NavigationLink { tabview()
+                    
+                } label: {
+                    
+                    ZStack {
+                        
+                        RoundedRectangle(cornerRadius: 40)
+                            .frame(width: 360, height: 85)
+                            .foregroundStyle(Color(red: 7/255, green: 28/255, blue: 24/255).gradient)
+                        
+                        Text("Get Started")
+                            .font(.title2)
+                            .fontDesign(.default)
+                            .fontWeight(.bold)
+                            .foregroundStyle(Color.white)
+                        
+                    }
+                }
+                .buttonStyle(.plain)
+                
+            }
+            .onAppear {
+                farmLogo = true
+            }
         }
-        .padding()
     }
+    
+    
 }
+
 
 #Preview {
     ContentView()
