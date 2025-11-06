725ee7d6580a76f4d3dacebfdcb2523e93bbc6b0
Amanda <soganileamanda@gmail.com>
2025-11-06 15:16:48 +0200

    modified several elements in the user interface

---
+++ FarmCare/Main Pages/theme.swift
@@ -0,0 +1,8 @@
+//
+//  theme.swift
+//  FarmCare
+//
+//  Created by Amanda Soganile on 4/11/2025.
+//
+
+import Foundation
---
+++ FarmCare/Main Pages/themeManger.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  FarmCare
+//
+//  Created by Amanda Soganile on 4/11/2025.
+//
+
+import Foundation
